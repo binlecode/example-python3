77d5f558df49d100f3351f6936708226630bf678
Bin Le <bin.le.code@gmail.com>
2019-10-14 08:55:50 -0400

    add enumerate usage

--- test_list_comprehension.py
+++ test_list_comprehension.py
@@ -8,3 +8,6 @@ print({x: x + 10 for x in range(3)})  # note the '{..}' wrapper to indicate a di
 
 # use inline filter
 print({x: x + 10 for x in range(10) if x % 2})  # only odd elements
+
+# get (idx, elm) from each iterator using built-in enumerate
+print({idx: x * 0-1 for idx, x in enumerate(range(10)) if not x % 2})  
