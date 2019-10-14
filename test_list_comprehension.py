484a778772aa8e726e3ddb64fe85af4f1c04b924
Bin Le <bin.le.code@gmail.com>
2019-10-14 08:53:13 -0400

    add inline filter in list comprehension

--- test_list_comprehension.py
+++ test_list_comprehension.py
@@ -5,3 +5,6 @@ print([x + y for x in range(3) for y in [n * 10 for n in range(4)]])
 
 # dictionary generation from list comprehension, this is new in python 3.x
 print({x: x + 10 for x in range(3)})  # note the '{..}' wrapper to indicate a dictionary
+
+# use inline filter
+print({x: x + 10 for x in range(10) if x % 2})  # only odd elements
